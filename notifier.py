b1d0f535b270fada328b5605e5dce8d6098596a1
Ueno Gohong <taka20030902@gmail.com>
2025-04-24 13:23:21 +0900

    add : .env 파일 추가 및 슬랙 웹훅 URL 환경 변수로 변경

--- notifier.py
+++ notifier.py
@@ -1,13 +1,17 @@
-# notifier.py
 import requests
+import os
+from dotenv import load_dotenv
 
-SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
+# .env 파일을 로드
+load_dotenv()
+
+SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
 
 def send_slack_message(message: str):
     payload = {"text": message}
     try:
         response = requests.post(SLACK_WEBHOOK_URL, json=payload)
         if response.status_code != 200:
-            print(f"Slack알림 실페: {response.text}")
+            print(f"Slack 알림 실패: {response.text}")
     except Exception as e:
-        print(f"Slack정송중 오류 발생: {e}")
+        print(f"Slack 전송 중 오류 발생: {e}")
